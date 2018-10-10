229b61e448101d060485ec61808b540b90ba02fd
sunzhuanzhuan <sunzhuanzhuan@weiboyi.com>
2018-10-10 11:44:29 +0800

    详情页底部重新布局

--- pages/detail/detail.js
+++ pages/detail/detail.js
@@ -257,8 +257,89 @@ Page({
         if (e.detail.formId) {
             gdt.collectTplMessageQuotaByForm(e.detail.formId);
         }
-        // console.log( e.detail.formId)
-        // this.setData({
-        // formId: e.detail.formId }) 
-    }
+    },
+    handlePoster: function (cardInfo, avaterSrc, codeSrc) {
+        wx.showLoading({
+          title: '生成中...',
+          mask: true,
+        })
+        var that = this;
+        const ctx = wx.createCanvasContext('myCanvas');
+        var width = "";
+        wx.createSelectorQuery().select('#canvas-container').boundingClientRect(function (rect) {
+          var height = rect.height;
+          var right = rect.right;
+          width = rect.width * 0.8;
+          var left = rect.left + 5;
+          ctx.setFillStyle('#fff');
+          ctx.fillRect(0, 0, rect.width, height);
+    
+          //头像
+          if (avaterSrc) {
+            ctx.drawImage(avaterSrc, left, 20, width, width);
+            ctx.setFontSize(14);
+            ctx.setFillStyle('#fff');
+            ctx.setTextAlign('left');
+          }
+    
+          if (cardInfo.TagList.length > 0) {
+            ctx.fillText(cardInfo.TagList[0].TagName, left + 20, width - 4); //标签
+            const metrics = ctx.measureText(cardInfo.TagList[0].TagName); //测量文本信息
+            ctx.stroke();
+            ctx.rect(left + 10, width - 20, metrics.width + 40, 25);
+            ctx.setFillStyle('rgba(255,255,255,0.4)');
+            ctx.fill();
+          }
+    
+          if (cardInfo.CardInfo.Name) {
+            ctx.setFontSize(14);
+            ctx.setFillStyle('#000');
+            ctx.setTextAlign('left');
+            ctx.fillText(cardInfo.CardInfo.Name, left, width + 60); //姓名
+          }
+    
+          if (cardInfo.CardInfo.Position) {
+            ctx.setFontSize(12);
+            ctx.setFillStyle('#666');
+            ctx.setTextAlign('left');
+            ctx.fillText(cardInfo.CardInfo.Position, left, width + 85); //职位
+          }
+    
+          if (cardInfo.CardInfo.Mobile) {
+            ctx.setFontSize(12);
+            ctx.setFillStyle('#666');
+            ctx.setTextAlign('left');
+            ctx.fillText(cardInfo.CardInfo.Mobile, left, width + 105); //电话
+          }
+    
+          if (cardInfo.CardInfo.Company) {
+            // 公司名称
+            const CONTENT_ROW_LENGTH = 24; // 正文 单行显示字符长度
+            let [contentLeng, contentArray, contentRows] = that.textByteLength(cardInfo.CardInfo.Company, CONTENT_ROW_LENGTH);
+            ctx.setTextAlign('left');
+            ctx.setFillStyle('#000');
+            ctx.setFontSize(10);
+            let contentHh = 22 * 1;
+            for (let m = 0; m < contentArray.length; m++) {
+              ctx.fillText(contentArray[m], left, width + 150 + contentHh * m);
+            }
+          }
+    
+          //  绘制二维码cardInfo.CardInfo.QrCode
+          if (codeSrc) {
+            ctx.drawImage(codeSrc, left + 150, width + 40, width / 3, width / 3)
+            ctx.setFontSize(10);
+            ctx.setFillStyle('#000');
+            ctx.setTextAlign('right');
+            ctx.fillText("微信扫码或长按识别", left + 235, width + 150);
+          }
+    
+        }).exec()
+    
+        setTimeout(function () {
+          ctx.draw();   //这里有个需要注意就是，这个方法是在绘制完成之后在调用，不然容易其它被覆盖。
+          wx.hideLoading();
+        }, 1000)
+    
+      },
 })
