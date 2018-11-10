684c338024a55d1521d0a5520132aa343cc10d90
sunzhuanzhuan <sunzhuanzhuan@weiboyi.com>
2018-11-10 18:02:12 +0800

    save

--- pages/index/index.js
+++ pages/index/index.js
@@ -29,7 +29,17 @@ Page({
         //是否正在播放
         listening:false,
         listenIndexCurrent:0,
-        listenTablistCurrent:0
+        listenTablistCurrent:0,
+        //推荐的top 
+        imgUrls: [
+            'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+            'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
+            'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
+        ],
+        indicatorDots: false,
+        autoplay: false,
+        interval: 5000,
+        duration: 1000
     },
 
     //听力
