34faf6ccf038e159c27d5475d0ea6bb0920bbaca
Yanlong Wang <wangyanlong@weiboyi.com>
2018-12-24 10:06:03 +0800

    Fix recommendation switch

--- pages/index/index.js
+++ pages/index/index.js
@@ -483,7 +483,7 @@ Page({
 				loadding:true
 			})
 
-			if (app.topListEnabled === false) {
+      if (app.toplistEnabled !== false) {
 				gdt.magicListItemLoadMore('topScoreds').then((res) => {
 					const theList = app.listIndex['topScoreds'];
 					let arr = theList.items.splice(0, 3);
