d75949282d58e1fb256b99f0e0564d5c9179c5f1
sunzhuanzhuan <sunzhuanzhuan@weiboyi.com>
2018-12-24 16:13:22 +0800

    mock数据

--- pages/check/check.js
+++ pages/check/check.js
@@ -2,12 +2,14 @@
 
 let app = getApp().globalData;
 const gdt = app.applicationDataContext;
-
+const data  = require('../../data/mock')
 Page({
 	data: {
 		url: '',
 		reportSubmit: true
 	},
-
+	onShow:function(){
+		console.log(data.data)
+	}
 
 })
