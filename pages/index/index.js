d31b48fcf722c4ff110ba4a5e43cf2df46cacb5a
sunzhuanzhuan <sunzhuanzhuan@weiboyi.com>
2018-12-27 14:35:51 +0800

    解决上拉刷新和下拉加载的报错

--- pages/index/index.js
+++ pages/index/index.js
@@ -540,18 +540,10 @@ Page({
 		wx.showLoading({title:'加载中',icon:'loadding'})
 		const currentListInstance = this.data.lists[this.data.currentTabIndex];
 		if (currentListInstance) {
-			if (currentListInstance._id === 'topScoreds') {
-				gdt.magicListItemLoadMore(currentListInstance._id).then(() => {
-					wx.hideLoading()
-					gdt.track('item-list-load-more', { listId: currentListInstance._id, title: currentListInstance.title, acc: currentListInstance.items.length });
-				});
-			} else {
-				gdt.magicListItemLoadMore(currentListInstance._id).then(() => {
-					wx.hideLoading()
-					gdt.track('item-list-load-more', { listId: currentListInstance._id, title: currentListInstance.title, acc: currentListInstance.items.length });
-				});
-			}
-
+			gdt.magicListItemLoadMore(currentListInstance._id).then(() => {
+				wx.hideLoading()
+				gdt.track('item-list-load-more', { listId: currentListInstance._id, title: currentListInstance.title, acc: currentListInstance.items.length });
+			});
 		}
 	},
 	//下拉刷新
@@ -560,31 +552,13 @@ Page({
 		wx.showLoading({title:'加载中',icon:'loadding'})
 		let that = this;
 		if (currentListInstance) {
-			if (currentListInstance._id === 'topScoreds') {
-				gdt.magicListItemLoadLatest(currentListInstance._id).then((res) => {
-					wx.hideLoading()
-					let oldRes = JSON.parse(JSON.stringify(res));
-					const theList = app.listIndex['topScoreds'];
-					let arr = theList.items.splice(0, 3);
-
-					if (app.lists[0] !== app.listIndex['topScoreds']) {
-						app.lists.unshift(app.listIndex['topScoreds']);
-					}
-
-					this.setData({
-						lists: app.lists,
-						imgUrls: arr
-					})
-				});
-			} else {
-				gdt.magicListItemLoadLatest(currentListInstance._id).then(() => {
-					wx.hideLoading()
-					gdt.track('item-list-refresh', { listId: currentListInstance._id, title: currentListInstance.title });
-					setTimeout(() => {
-						wx.stopPullDownRefresh();
-					}, 500);
-				});
-			}
+			gdt.magicListItemLoadLatest(currentListInstance._id).then(() => {
+				wx.hideLoading()
+				gdt.track('item-list-refresh', { listId: currentListInstance._id, title: currentListInstance.title });
+				setTimeout(() => {
+					wx.stopPullDownRefresh();
+				}, 500);
+			});
 		}
 	},
 	getFormID: function (e) {
